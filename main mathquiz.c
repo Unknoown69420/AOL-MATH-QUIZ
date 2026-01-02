12b37a566f05fe3d07fb547bae195250248219be
Titus Krislangga <titusjanevenk@gmail.com>
2026-01-02 23:52:34 +0700

    Update main mathquiz.c

--- main mathquiz.c
+++ main mathquiz.c
@@ -107,10 +107,7 @@ void displayLeaderboard() {
         return;
     }
 
-    while (fscanf(fp, "%s %d", players[count].name, &players[count].score) != EOF) {
-        count ++;
-    }
-    fclose(fp);
+    
 
     for (int i = 0; i < count - 1; i++) {
         for (int j = 0; j < count - i - 1; j++) {
