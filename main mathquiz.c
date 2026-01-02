ddfdd8ddcca72edefb5308a029ddb8dc3c89f5b8
stevenagustianto <stevenagustianto13@gmail.com>
2026-01-02 21:05:12 +0700

    Update main mathquiz.c

--- main mathquiz.c
+++ main mathquiz.c
@@ -43,7 +43,51 @@ case 3: // Level Sulit (campuran operasi dengan bilangan besar)
     }
 }
 
+//fungsi biar bisa nerima jawaban dari pengguna
+int getUserAnswer (int a, int b, char op){
+  int answer;
+  printf("Berapakah %d %c %d", a, op, b);
+  scanf("%d, &answer");
+  return answer;
+}
+  
+//fungsi buat meriksa jawaban
+int checkAnswer (int userAnswer, int correctAnswer){
+  return userAnswer == correctAnswer;
+}
 
+//fungsi untuk nampilin skor akhir per level 
+void showScore(int score, int total, int level){
+  printf("\n=== HASIL LEVEL ===\n", level);
+  printf("Skor Akhir Kamu: %d dari %d\n", score, total);
+  if (score == total){
+    printf ("Sempurna!\n");
+  } else if (score >= total/2){
+    printf("Lumayan, kamu lulus.\n");
+  } else {
+    printf ("Belajar lagi...\n");
+  }
+}
+
+int main (){
+srand (time(NULL));
+    int totalQuestions = 5;
+    int level, retry;
+    int totalScore = 0;
+    int totalPlayed = 0;
+
+    printf("===KUIS MATEMATIKA===\n");
+    do { 
+        printf("\nPilih Level Kesulitan:\n");
+        printf("1. Mudah\n2. Sedang\n3. Sulit\n");
+        printf("{{Pilih (1-3): ");
+        scanf("%d, &level);
+
+        if (level < 1 || level > 3) {
+            printf("Pilihan ga ada");
+    } else {
+        int score = 0;
+        printf ("\n---Level %d ---\n", level);
 
 int main (){
 
