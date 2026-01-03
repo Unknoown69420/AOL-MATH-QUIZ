61694b305c701fdd79d76703913bae3d5b936cb1
Titus Krislangga <titusjanevenk@gmail.com>
2026-01-03 17:33:45 +0700

    Update main mathquiz.c

--- main mathquiz.c
+++ main mathquiz.c
@@ -53,7 +53,8 @@ int main() {
                 continue;
             }
 
-            
+            numA = rand() % (rangeMax - rangeMin + 1) + rangeMin;
+            numB = rand() % (rangeMax - rangeMin + 1) + rangeMin;
 
             if (difficulty == 1) {
                 correctAnswer = numA + numB;
